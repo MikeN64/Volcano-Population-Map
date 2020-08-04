617f03f8b7bd854f64c0317fbfff94525fba662f
MikeN64 <nguyen.michael.van@gmail.com>
2020-08-04 16:50:10 -0400

    Used pandas and volcanoes data to display points on the map

--- map1.py
+++ map1.py
@@ -1,11 +1,21 @@
 import folium
+import pandas
 
-map = folium.Map(location=[38.58, -99.09], zoom_start=6, tiles="Stamen Terrain")
+data = pandas.read_csv("Volcanoes.csv")
+lats = list(data["LAT"])
+lons = list(data["LON"])
+names = list(data["NAME"])
+elevs = list(data["ELEV"])
 
-feature_group = folium.FeatureGroup(name="My Map")
+START_POINT = (38.58, -99.09)
 
-for coordinates in [[38.2, -99.1], [39.2, -97.1]]:
-    feature_group.add_child(folium.Marker(location=coordinates, popup="Hi! I am a marker", icon=folium.Icon(color="green")))
+map = folium.Map(location=START_POINT, zoom_start=6, tiles="Stamen Terrain")
+
+feature_group = folium.FeatureGroup(name="Volcanos Map")
+
+for lat, lon, name, elev in zip(lats, lons, names, elevs):
+    popup_str = name + "\n" + str(elev) + "m"
+    feature_group.add_child(folium.Marker(location=(lat, lon), popup=popup_str, icon=folium.Icon(color="green")))
 
 map.add_child(feature_group)
 
