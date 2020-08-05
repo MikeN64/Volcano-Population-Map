f0adba91a7d9959a2e61d86a60e07005d0611e11
MikeN64 <nguyen.michael.van@gmail.com>
2020-08-05 16:31:34 -0400

    Stylized popup to contain volcano info and link

--- map1.py
+++ map1.py
@@ -9,13 +9,18 @@ elevs = list(data["ELEV"])
 
 START_POINT = (38.58, -99.09)
 
-map = folium.Map(location=START_POINT, zoom_start=6, tiles="Stamen Terrain")
+html = """<h4>Volcano Information</h4>
+<a href="https://www.google.com/search?q={}" target="_blank">{}</a><br>
+Height: {} m
+"""
+
+map = folium.Map(location=START_POINT, zoom_start=5, tiles="Stamen Terrain")
 
 feature_group = folium.FeatureGroup(name="Volcanos Map")
 
 for lat, lon, name, elev in zip(lats, lons, names, elevs):
-    popup_str = name + "\n" + str(elev) + "m"
-    feature_group.add_child(folium.Marker(location=(lat, lon), popup=popup_str, icon=folium.Icon(color="green")))
+    iframe = folium.IFrame(html=html.format(name, name, elev), width=200, height=100)
+    feature_group.add_child(folium.Marker(location=(lat, lon), popup=folium.Popup(iframe), icon=folium.Icon(color="green")))
 
 map.add_child(feature_group)
 
