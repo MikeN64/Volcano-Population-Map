ae8286a0aaf956db06f8330db3c4310945b0761a
MikeN64 <nguyen.michael.van@gmail.com>
2020-08-05 19:26:46 -0400

    Added style_population function

--- map1.py
+++ map1.py
@@ -25,7 +25,23 @@ def produce_marker_color(elev):
     if elev >= HIGH:
         return "red"
     return "gray"
-    
+
+def style_population(polygon):
+    LOW = 10000000
+    HIGH = 20000000
+
+    population = polygon["properties"]["POP2005"]
+    styles = {}
+
+    if population <= LOW:
+        styles = {"fillColor": "green"}
+    elif LOW < population < HIGH:
+        styles = {"fillColor": "orange"}
+    elif population >= HIGH:
+        styles = {"fillColor": "red"}
+
+    return styles 
+
 map = folium.Map(location=START_POINT, zoom_start=5, tiles="Stamen Terrain")
 
 feature_group = folium.FeatureGroup(name="Volcanos Map")
@@ -35,7 +51,7 @@ for lat, lon, name, elev in zip(lats, lons, names, elevs):
     circle_marker = folium.CircleMarker(location=(lat, lon), popup=folium.Popup(iframe), fill_color=produce_marker_color(elev), color="grey", fill_opacity=0.7)
     feature_group.add_child(circle_marker)
 
-geo_json = folium.GeoJson(data=open("world.json", "r", encoding="utf-8-sig").read(), style_function=lambda polygon: {"fillColor": "yellow"})
+geo_json = folium.GeoJson(data=open("world.json", "r", encoding="utf-8-sig").read(), style_function=style_population)
 feature_group.add_child(geo_json)
 
 map.add_child(feature_group)
